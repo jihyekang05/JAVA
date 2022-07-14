87c55093307da108db3c7b42418b239050341eff
JIHYE KANG <62206966+jihyekang05@users.noreply.github.com>
2022-07-14 15:55:19 +0900

    Create ScoreMain.java

---
+++ 객체지향(성적처리)/ScoreMain.java
@@ -0,0 +1,13 @@
+package 성적처리;
+
+public class ScoreMain {
+	public static void main(String[] args) {
+		
+		ScoreManager mgr = new ScoreManager();
+		mgr.init();
+		mgr.input();
+		mgr.output();
+		
+		
+	}
+}
